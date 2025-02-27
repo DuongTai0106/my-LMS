c29faabd0bb539b253c39414eeaf1d2e2385138a
DuongTai0106 <duongthanhtai0106@gmail.com>
2025-02-27 14:37:43 +0700

    done client and first commit server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/mongodb.js";
+import { clerkWebhooks } from "./controllers/webhooks.js";
+
+//Express
+const app = express();
+
+//Connect DB
+await connectDB();
+
+//middlwares
+app.use(cors());
+
+//Routes
+app.post("/clerk", express.json(), clerkWebhooks);
+
+//Port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
